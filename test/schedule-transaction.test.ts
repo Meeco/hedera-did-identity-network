b184d3f79b6ca8d01ead1f1417d628d9c8ff4375
elena <elena.murova@meeco.me>
2022-02-25 09:57:33 +1100

    feat: Update a schedule transaction test.

--- test/schedule-transaction.test.ts
+++ test/schedule-transaction.test.ts
@@ -5,96 +5,103 @@ import {
     ScheduleId,
     ScheduleCreateTransaction,
     ScheduleSignTransaction,
-    ScheduleDeleteTransaction,
     TransferTransaction,
-    Hbar
+    Hbar,
+    ScheduleInfoQuery,
+    TransactionId
 
 } from "@hashgraph/sdk";
+
 require("dotenv").config();
 
 describe("Schedule transaction", async () => {
     const OPERATOR_ID = process.env.HEDERA_OPERATOR_ID;
+    const OPERATOR_ID1 = process.env.HEDERA_OPERATOR_ID1;
     const OPERATOR_KEY = process.env.HEDERA_OPERATOR_KEY;
+    const OPERATOR_KEY1 = process.env.HEDERA_OPERATOR_KEY1;
     const NETWORK = process.env.HEDERA_NETWORK;
     const MIRROR_PROVIDER = ["hcs." + NETWORK + ".mirrornode.hedera.com:5600"];
 
     const privateKeySigner1 = PrivateKey.generate();
     const adminKey = PrivateKey.generate();
     // @ts-ignore
-    const operatorId = AccountId.fromString(OPERATOR_ID);
+    const senderAccount = AccountId.fromString(OPERATOR_ID);
     // @ts-ignore
-    const operatorKey = PrivateKey.fromString(OPERATOR_KEY);
+    const recipientAccount = AccountId.fromString(OPERATOR_ID1);
+    // @ts-ignore
+    const senderKey = PrivateKey.fromString(OPERATOR_KEY);
+    // @ts-ignore
+    const recipientKey = PrivateKey.fromString(OPERATOR_KEY1);
     let client = Client.forTestnet();
     client.setMirrorNetwork(MIRROR_PROVIDER);
-    client.setOperator(operatorId, operatorKey);
+    client.setOperator(senderAccount, senderKey);
     beforeAll(async () => {
 
     });
 
-    it("Create scheduleID", async () => {
-        const scheduleID = new ScheduleId(0, 0, 10);
-        console.log(scheduleID)
-    });
     it("Create a schedule transaction", async () => {
-         let transactionToSchedule = new TransferTransaction()
-            .addHbarTransfer(operatorId, Hbar.fromTinybars(-1))
-            .addHbarTransfer(operatorId, Hbar.fromTinybars(1));
+        //Create a transaction to schedule
+        const transaction = new TransferTransaction()
+            .addHbarTransfer(senderAccount, Hbar.fromTinybars(-1))
+            .addHbarTransfer(recipientAccount, Hbar.fromTinybars(1));
 
-        //Create a schedule transaction
-        const transaction = new ScheduleCreateTransaction()
-            .setScheduledTransaction(transactionToSchedule);
-            // .setAdminKey(operatorKey)
-            // .setPayerAccountId(operatorId);
+        //Schedule a transaction
+        const scheduleTransaction = await new ScheduleCreateTransaction()
+            .setScheduledTransaction(transaction)
+            .execute(client);
 
-        //Sign with the client operator key and submit the transaction to a Hedera network
+        //Get the receipt of the transaction
+        const receipt = await scheduleTransaction.getReceipt(client);
+
+        //Get the schedule ID
         // @ts-ignore
-        const txResponse = await transaction.execute(client);
+        const scheduleId: ScheduleId = receipt.scheduleId;
+        console.log("The schedule ID is " + scheduleId);
 
-        // //Request the receipt of the transaction
-        // // @ts-ignore
-        // const receipt = await txResponse.getReceipt(client);
-        //
-        // //Get the schedule ID
-        // const scheduleId = receipt.scheduleId;
-        // console.log("The schedule ID of the schedule transaction is " + scheduleId);
-    });
-    it("Sign a scheduled transaction", async () => {
-        const scheduleID = new ScheduleId(0, 0, 10);
-        // Create the transaction
-        const transaction = await new ScheduleSignTransaction()
-            .setScheduleId(scheduleID)
+        //Get the scheduled transaction ID
+        const scheduledTxId = receipt.scheduledTransactionId;
+        console.log("The scheduled transaction ID is " + scheduledTxId);
+
+        //Submit the first signature
+        const signature1 = await (await new ScheduleSignTransaction()
+            .setScheduleId(scheduleId)
             .freezeWith(client)
-            .sign(operatorKey);
-            // .sign(privateKeySigner1);
+            .sign(senderKey))
+            .execute(client);
 
-        //Sign with the client operator key to pay for the transaction and submit to a Hedera network
+        //Verify the transaction was successful and submit a schedule info request
+        const receipt1 = await signature1.getReceipt(client);
+        console.log("The transaction status is " + receipt1.status.toString());
+
+        const query1 = await new ScheduleInfoQuery()
+            .setScheduleId(scheduleId)
+            .execute(client);
+
+        //Confirm the signature was added to the schedule
+        console.log(query1);
+
+        //Submit the second signature
+        const signature2 = await (await new ScheduleSignTransaction()
+            .setScheduleId(scheduleId)
+            .freezeWith(client)
+            .sign(recipientKey))
+            .execute(client);
+
+        //Verify the transaction was successful
+        const receipt2 = await signature2.getReceipt(client);
+        console.log("The transaction status " + receipt2.status.toString());
+
+        //Get the schedule info
+        const query2 = await new ScheduleInfoQuery()
+            .setScheduleId(scheduleId)
+            .execute(client);
+
+        console.log(query2);
+
+        //Get the scheduled transaction record
         // @ts-ignore
-        const txResponse = await transaction.execute(client);
+        const scheduledTxRecord = await TransactionId.fromString(scheduledTxId.toString()).getRecord(client);
+        console.log("The scheduled transaction record is: " + scheduledTxRecord);
 
-        // //Get the receipt of the transaction
-        // // @ts-ignore
-        // const receipt = await txResponse.getReceipt(client);
-        //
-        // //Get the transaction status
-        // const transactionStatus = receipt.status;
-        // console.log("The transaction consensus status is " + transactionStatus);
     });
-    // it("Delete a scheduled transaction", async () => {
-    //     const scheduleID = new ScheduleId(0, 0, 10);
-    //     //Create the transaction and sign with the admin key
-    //     const transaction = await new ScheduleDeleteTransaction()
-    //         .setScheduleId(scheduleID)
-    //         .freezeWith(client)
-    //         .sign(adminKey);
-    //
-    //     //Sign with the operator key and submit to a Hedera network
-    //     const txResponse = await transaction.execute(client);
-    //
-    //     //Get the transaction receipt
-    //     const receipt = await txResponse.getReceipt(client);
-    //
-    //     //Get the transaction status
-    //     const transactionStatus = receipt.status;
-    //     console.log("The transaction consensus status is " + transactionStatus);
-    // });
 });
