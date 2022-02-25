2f257561931deaeee249067929a615882b120919
elena <elena.murova@meeco.me>
2022-02-25 15:39:07 +1100

    feat: Add the log file of the working example.

--- test/schedule-transaction.test.ts
+++ test/schedule-transaction.test.ts
@@ -7,7 +7,9 @@ import {
     ScheduleSignTransaction,
     TransferTransaction,
     Hbar,
-    ScheduleInfoQuery
+    ScheduleInfoQuery,
+    ScheduleDeleteTransaction,
+    PublicKey
 } from "@hashgraph/sdk";
 
 require("dotenv").config();
@@ -16,7 +18,9 @@ describe("Schedule transaction", async () => {
     const OPERATOR_ID = process.env.HEDERA_OPERATOR_ID;
     const OPERATOR_ID1 = process.env.HEDERA_OPERATOR_ID1;
     const OPERATOR_KEY = process.env.HEDERA_OPERATOR_KEY;
+    const OPERATOR_KEY_P = process.env.HEDERA_OPERATOR_KEY_P;
     const OPERATOR_KEY1 = process.env.HEDERA_OPERATOR_KEY1;
+    const OPERATOR_KEY1_P = process.env.HEDERA_OPERATOR_KEY1_P;
     const NETWORK = process.env.HEDERA_NETWORK;
     const MIRROR_PROVIDER = ["hcs." + NETWORK + ".mirrornode.hedera.com:5600"];
 
@@ -27,7 +31,11 @@ describe("Schedule transaction", async () => {
     // @ts-ignore
     const senderKey = PrivateKey.fromString(OPERATOR_KEY);
     // @ts-ignore
+    const senderKeyP = PublicKey.fromString(OPERATOR_KEY_P);
+    // @ts-ignore
     const recipientKey = PrivateKey.fromString(OPERATOR_KEY1);
+    // @ts-ignore
+    const recipientKeyP = PublicKey.fromString(OPERATOR_KEY1_P);
     let client = Client.forTestnet();
     client.setMirrorNetwork(MIRROR_PROVIDER);
     client.setOperator(senderAccount, senderKey);
@@ -38,8 +46,8 @@ describe("Schedule transaction", async () => {
     it("Create a schedule transaction", async () => {
         //Create a transaction to schedule
         const transaction = new TransferTransaction()
-            .addHbarTransfer(senderAccount, Hbar.fromTinybars(-1))
-            .addHbarTransfer(recipientAccount, Hbar.fromTinybars(1));
+            .addHbarTransfer(senderAccount, Hbar.fromTinybars(6))
+            .addHbarTransfer(recipientAccount, Hbar.fromTinybars(-6));
 
         //Schedule a transaction
         const scheduleTransaction = await new ScheduleCreateTransaction()
@@ -50,20 +58,12 @@ describe("Schedule transaction", async () => {
         const receipt = await scheduleTransaction.getReceipt(client);
 
         //Get the schedule ID
-        // @ts-ignore
-        const scheduleId: ScheduleId = receipt.scheduleId;
-        console.log("The schedule ID is " + scheduleId);
+        const scheduleId = receipt.scheduleId ? receipt.scheduleId : " ";
+        console.log("The schedule ID is " +scheduleId);
 
-        //Submit the first signature
-        const signature1 = await (await new ScheduleSignTransaction()
-            .setScheduleId(scheduleId)
-            .freezeWith(client)
-            .sign(senderKey))
-            .execute(client);
-
-        //Verify the transaction was successful and submit a schedule info request
-        const receipt1 = await signature1.getReceipt(client);
-        console.log("The transaction status is " + receipt1.status.toString());
+        //Get the scheduled transaction ID
+        const scheduledTxId = receipt.scheduledTransactionId;
+        console.log("The scheduled transaction ID is " +scheduledTxId);
 
         const query1 = await new ScheduleInfoQuery()
             .setScheduleId(scheduleId)
@@ -71,6 +71,7 @@ describe("Schedule transaction", async () => {
 
         //Confirm the signature was added to the schedule
         console.log(query1);
+        setTimeout(() => {  console.log("Slept"); }, 5000);
 
         //Submit the second signature
         const signature2 = await (await new ScheduleSignTransaction()
@@ -81,13 +82,7 @@ describe("Schedule transaction", async () => {
 
         //Verify the transaction was successful
         const receipt2 = await signature2.getReceipt(client);
-        console.log("The transaction status " + receipt2.status.toString());
+        console.log("The transaction status " +receipt2.status.toString());
 
-        //Get the schedule info
-        const query2 = await new ScheduleInfoQuery()
-            .setScheduleId(scheduleId)
-            .execute(client);
-
-        console.log(query2);
     });
 });
