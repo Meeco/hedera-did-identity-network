d38b876b29295c025feefdc24cf41de0eb25d7eb
Vijay Shiyani <vshiyani@meeco.me>
2022-06-27 13:38:54 +1000

    connect to mongodb from local machine.

--- src/services/connection.service.ts
+++ src/services/connection.service.ts
@@ -8,10 +8,10 @@ export const getMongoose = () => {
 
 export const connectWithRetry = async () => {
   console.log(
-    `Attempting MongoDB connection (will retry if needed) mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+    `Attempting MongoDB connection (will retry if needed) mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin&ssl=false`
   );
 
   await mongoose.connect(
-    `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+    `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin&ssl=false`
   );
 };
